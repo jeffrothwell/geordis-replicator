cc16bd759a48c2debbc95684846925111169b061
jeffrothwell <rothwelljeff@gmail.com>
2017-12-19 16:29:06 -0500

    complete ep 6

--- episode-06/transporter.rb
+++ episode-06/transporter.rb
@@ -1,3 +1,4 @@
+require "pry"
 # The Transporter moves the glass around
 # the ship and ingredients into the glass.
 # It requires power to work properly.
@@ -8,7 +9,7 @@ class Transporter
     @enterprise = enterprise
     @power = false
     connect_to_power
-    obriens_repairs_transporter
+    # obriens_repairs_transporter
   end
 
   def connect_to_power
@@ -20,6 +21,7 @@ class Transporter
   end
 
   def energize(obj, from, to)
+    # binding.pry
     return unless @power
 
     if @enterprise.reactor.draw_power(3)
