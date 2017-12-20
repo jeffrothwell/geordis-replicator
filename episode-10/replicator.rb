8fcce296d450f8b7d713dc2c32d05fa19f269426
jeffrothwell <rothwelljeff@gmail.com>
2017-12-19 21:24:20 -0500

    complete ep 10

--- episode-10/replicator.rb
+++ episode-10/replicator.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -44,7 +45,7 @@ class Replicator
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
-
+    # binding.pry
     # This transports a glass from the cupboard to inside the replicator.
     # If this method is successful, it will return the glass that was
     # transported and @inside_replicator will contain the glass
@@ -64,7 +65,8 @@ class Replicator
     #   glass_inside_replicator
     # And then to see what's inside the glass, use:
     #   glass_inside_replicator.inside.contents
-    transport_ingredients_to_glass
+    transport_ingredients_to_glass  # returned array of ingredients
+    # but after running this, glass just had nil in it's array of contents
 
     # This methods mixes the ingredients in the glass around.
     # It returns nil, even if successful, but if you look at:
@@ -104,7 +106,7 @@ class Replicator
 
   # This transports ingredients into the glass.
   def transport_ingredients_to_glass
-
+    # binding.pry
     # Abort if there is no glass inside the replicator.
     return unless glass_inside_replicator
 
